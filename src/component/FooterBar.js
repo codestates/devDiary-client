764c5d303df961441498dfd2ba7a84f41cf9cf84
chaerang2 <codbs54@gmail.com>
2020-12-23 10:27:13 +0900

    [Client] / #88 / edit: FooterBar.js 내용은 정해지지않아 기능만 구현해둠

---
+++ src/component/FooterBar.js
@@ -0,0 +1,15 @@
+function FooterBar(){
+    return(
+        <>
+    <span>id</span>
+    <input type="text" ></input>
+    <span>password</span>
+    <input type="password" ></input>
+    <button>로그인</button>
+    <button>회원가입</button>
+    <button>소셜로그인</button>
+     </>
+    )
+}
+
+export default FooterBar;
