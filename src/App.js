13cbdd9d5eb844a89c9ec3f71453bef490dde148
chaerang2 <codbs54@gmail.com>
2020-12-23 14:46:18 +0900

    [Client]/ #29 / feature: 로그인기능(소셜로그인은 버튼만 구현하였음 상세url은 수정필요)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import axios from "axios";
 import './App.css';
+import Login from"./component/Login"
+import Hi from"./component/hi"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  state = {
+    isLogin: false,
+    userinfo: null,
+  };
+
+  handleResponseSuccess() {
+    axios.get('http://localhost:4000/userinfo')
+    .then(param=>{
+      this.setState({ isLogin: true, userinfo: param.data });
+    })
+  
+  }
+
+  render() {
+    const { isLogin, userinfo } = this.state;
+
+    return (
+      <div>
+      <Switch>
+
+        <Route
+          path='/login'
+          render={() => (
+            <Login handleResponseSuccess={this.handleResponseSuccess.bind(this)} />
+          )}
+        />
+        <Route exact path='/hi' render={() => <Hi />} />
+        <Route
+            path='/'
+            render={() => {
+              if (isLogin) {
+                return <Redirect to='/hi' />;
+              }
+              return <Redirect to='/login' />;
+            }}
+          />
+      </Switch>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
