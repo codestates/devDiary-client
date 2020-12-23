865b94523906ae83d2e3963b9e4a532785c42400
Hyunwoo Han <h900506@gmail.com>
2020-12-23 10:26:35 +0900

    [Client] / #88 / feature: MainPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import axios from "axios";
+
+import Main from "./component/Main";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLogin: false,
+    userinfo: null,
+  };
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
