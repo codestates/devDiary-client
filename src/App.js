1fc25f84abea6aa8531be50c9896087babb917d9
Hyunwoo Han <h900506@gmail.com>
2020-12-25 17:47:52 +0900

    [Client] / #45 / bug: some changes from merging

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import NavBar from "./component/NavBar"
 import {
   BrowserRouter as Router,
   Switch,
-  Route, 
-  Redirect, 
+  Route,
+  Redirect,
   withRouter
 } from "react-router-dom";
 import axios from "axios";
-import Login from"./component/Login"
-import Hi from"./component/hi"
+import Login from "./component/Login"
 import Main from "./component/Main";
 import SignUp from "./component/SignUp";
 
@@ -24,37 +23,33 @@ class App extends React.Component {
 
   handleResponseSuccess() {
     axios.get('http://localhost:4000/userinfo')
-    .then(param=>{
-      this.setState({ isLogin: true, userinfo: param.data });
-    })
-  
-  }            
-            
+      .then(param => {
+        this.setState({ isLogin: true, userinfo: param.data });
+      })
+
+  }
+
   render() {
     const { isLogin, userinfo } = this.state;
-    
+
     return (
       <>
         <h1>hi every one</h1>
         <NavBar isLogin={this.state.isLogin}></NavBar>
-      <Router>
-        <Switch>
-      <Route 
-          path='/login'
-          render={() => (
-            <Login handleResponseSuccess={this.handleResponseSuccess.bind(this)} />
-          )}
-        />
-        <Route exact path='/login' render={() => <Login />} />
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/user/signUp">
-            <SignUp />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+        <Router>
+          <Switch>
+            <Route
+              path='/login'
+              render={() => (
+                <Login handleResponseSuccess={this.handleResponseSuccess.bind(this)} />
+              )}
+            />
+            <Route exact path='/login' render={() => <Login />} />
+            <Route exact path='/login' render={() => <Main />} />
+            <Route exact path='/login' render={() => <SignUp />} />
+          </Switch>
+        </Router>
+      </>
     );
   }
 }
