7fc3f8b250884cfd4967a34e8202f408264bf2d7
chaerang2 <codbs54@gmail.com>
2020-12-28 03:01:36 +0900

    [Bug] / #49 / edit:NavBar.js, App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,45 @@
 import React from "react";
-
-import NavBar from "./component/NavBar"
 import {
   BrowserRouter as Router,
   Switch,
-  Route, 
-  Redirect, 
-  withRouter
+  Route
 } from "react-router-dom";
 import axios from "axios";
-import Login from"./component/Login"
-import Hi from"./component/hi"
+import NavBar from "./component/NavBar"
+import Login from "./component/Login"
 import Main from "./component/Main";
 import SignUp from "./component/SignUp";
-
 import "./App.css";
-
 class App extends React.Component {
   state = {
     isLogin: false,
     userinfo: null,
   };
-
   handleResponseSuccess() {
-    axios.get('http://localhost:4000/userinfo')
-    .then(param=>{
-      this.setState({ isLogin: true, userinfo: param.data });
-    })
-  
-  }            
-            
+    axios.get('http://localhost:4000/user/userinfo')
+      .then(param => {
+        this.setState({ isLogin: true, userinfo: param.data });
+      })
+  }
   render() {
-    const { isLogin, userinfo } = this.state;
-    
     return (
       <>
         <h1>hi every one</h1>
-        <NavBar isLogin={this.state.isLogin}></NavBar>
-      <Router>
-        <Switch>
-      <Route 
-          path='/login'
-          render={() => (
-            <Login handleResponseSuccess={this.handleResponseSuccess.bind(this)} />
-          )}
-        />
-        <Route exact path='/login' render={() => <Login />} />
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/user/signUp">
-            <SignUp />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+        <Router>
+          <NavBar isLogin={this.state.isLogin}></NavBar>
+          <Switch>
+            <Route path='/user/login'>
+              <Login handleResponseSuccess={this.handleResponseSuccess.bind(this)} />
+            </Route>
+            <Route exact path='/'>
+              <Main />
+            </Route>
+            <Route path='/user/signup'>
+              <SignUp />
+            </Route>
+          </Switch>
+        </Router>
+      </>
     );
   }
 }
