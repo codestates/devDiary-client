b8a09bb300b8afae7a46c57ce04b14ace4fb964c
Choe-Yun <cjh7687@gmail.com>
2021-01-02 13:04:46 +0900

    [Client]/#76/네비바 css 적용

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>hi every one</h1>
+        {/* <h1>hi every one</h1> */}
         <Router>
           {this.state.isLogin === true
             ? (
